b2d36a0a360cccc5347116c57f127bfa9470e92d
julien <julien.sebrien@gmail.com>
2013-07-21 07:32:53 +0200

    print current counter value

--- src/test/java/blog/hashmade/cassandra/CounterTest.java
+++ src/test/java/blog/hashmade/cassandra/CounterTest.java
@@ -1,28 +1,21 @@
 package blog.hashmade.cassandra;
 
-import java.math.BigDecimal;
-
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import blog.hashmade.cassandra.util.DbUtil;
-import blog.hashmade.cassandra.util.Pizza;
-import blog.hashmade.cassandra.util.PizzaBuilder;
-
-import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ConsistencyLevel;
 import com.datastax.driver.core.ExecutionInfo;
 import com.datastax.driver.core.Metadata;
-import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.ResultSetFuture;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.querybuilder.Delete;
-import com.datastax.driver.core.querybuilder.Insert;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.querybuilder.Select;
 import com.datastax.driver.core.querybuilder.Update;
 import com.datastax.driver.core.utils.UUIDs;
 
@@ -33,7 +26,7 @@ public class CounterTest {
   private static final String NODE_IP = "127.0.0.1";
 
   private Cluster cluster;
-  
+
   @Before
   public void setUp() throws Exception {
     cluster = Cluster.builder().addContactPoint(NODE_IP).build();
@@ -42,14 +35,14 @@ public class CounterTest {
   }
 
   @Test
-  public void testBound() throws Exception {
+  public void testCounter() throws Exception {
     ResultSet resultSet = null;
     ExecutionInfo info = null;
-    
+
     Session session = cluster.connect("pizzastore");
-    
+
     String pizzaId = UUIDs.random().toString();
-    
+
     LOGGER.info("Increment the counter");
     // Update 1 pizza
     Update updateStatement = QueryBuilder.update("stats");
@@ -60,6 +53,8 @@ public class CounterTest {
     info = resultSet.getExecutionInfo();
     Assert.assertNotNull(info);
 
+    printCounterValue(session, pizzaId);
+
     LOGGER.info("Decrement the counter");
     // Update 1 pizza
     updateStatement = QueryBuilder.update("stats");
@@ -69,25 +64,52 @@ public class CounterTest {
     resultSet = session.execute(updateStatement.getQueryString());
     info = resultSet.getExecutionInfo();
     Assert.assertNotNull(info);
-
+    
+    printCounterValue(session, pizzaId);
+    
+    updateStatement = QueryBuilder.update("stats");
+    updateStatement.where(QueryBuilder.eq("pizza_id", pizzaId));
+    updateStatement.with(QueryBuilder.incr("nb_ordered", 3));
+    
+    resultSet = session.execute(updateStatement.getQueryString());
+    info = resultSet.getExecutionInfo();
+    Assert.assertNotNull(info);
+    
+    printCounterValue(session, pizzaId);
+    
     LOGGER.info("Delete the counter");
-    Delete.Selection delSomeSelection = QueryBuilder.delete()
-      .column("nb_ordered");
+    Delete.Selection delSomeSelection = QueryBuilder.delete().column("nb_ordered");
     Delete deleteStatement = delSomeSelection.from("stats");
     deleteStatement.where(QueryBuilder.eq("pizza_id", pizzaId));
 
     resultSet = session.execute(deleteStatement.getQueryString());
     info = resultSet.getExecutionInfo();
     Assert.assertNotNull(info);
-    
+
     LOGGER.info("Try to recreate the counter");
     updateStatement = QueryBuilder.update("stats");
     updateStatement.where(QueryBuilder.eq("pizza_id", pizzaId));
     updateStatement.with(QueryBuilder.incr("nb_ordered"));
 
     resultSet = session.execute(updateStatement.getQueryString());
-    
+
     session.shutdown();
+  }
+
+  public void printCounterValue(Session session, String pizzaId) {
+    ResultSet resultSet;
+    Select selectStatement = QueryBuilder.select()
+      .column("nb_ordered")
+      .from("stats")
+      .where(QueryBuilder.eq("pizza_id", pizzaId))
+      .limit(1);
+    selectStatement.setConsistencyLevel(ConsistencyLevel.ONE).enableTracing();
+
+    resultSet = session.execute(selectStatement.getQueryString());
+    Row row = resultSet.one();
+    if (row != null) {
+      LOGGER.info("Current counter value is: " + row.getLong("nb_ordered"));
+    }
   }
 
   @After
